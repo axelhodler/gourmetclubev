e7f060909238c6a4f045b7c4f698292cc56cef3e
xorrr <derxor@gmail.com>
2015-06-04 13:20:51 +0200

    add places and their names as labels

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
+var places = [
+  {"name": "87", "address" : "König-Karl-Straße 87, Stuttgart"},
+  {"name": "Gigis Burgerbar", "address" : "Theodor-Heuss-Strasse 34, 70174 Stuttgart"},
+  {"name": "Bonnie and Clyde", "address": "Heinrich Baumann Str. 24, 70190 Stuttgart"},
+  {"name": "Madagascar", "address": "Filderstrasse 61, 70180 Stuttgart"},
+  {"name": "Ceylonas", "address": "Reinsburgstraße 74 70178 Stuttgart"},
+  {"name": "Tshito Gwrito", "address": "Heinrich-Baumann-Straße 23, 70190 Stuttgart"},
+  {"name": "Ebony", "address" : "Herzogstrasse 11, 70176 Stuttgart"},
+  {"name": "Gerüchteküche-Stuttgart", "address": "Rotebühlstrasse 159, 70197 Stuttgart"}
+];
+
 var geocoder;
 var map;
 var address ="Stuttgart, Germany";
@@ -18,27 +29,34 @@ function initialize() {
       if (status == google.maps.GeocoderStatus.OK) {
         if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
           map.setCenter(results[0].geometry.location);
-
-          var infowindow = new google.maps.InfoWindow(
-            { content: '<b>'+address+'</b>',
-              size: new google.maps.Size(150,50)
-            });
-
-          var marker = new google.maps.Marker({
-            position: results[0].geometry.location,
-            map: map,
-            title:address
-          });
-          google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map,marker);
-          });
-
         } else {
           alert("No results found");
         }
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
+    });
+    places.forEach(function(places) {
+      geocoder.geocode( { 'address': places.address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+            map.setCenter(results[0].geometry.location);
+
+            var marker = new MarkerWithLabel({
+              position: results[0].geometry.location,
+              map: map,
+              title: places.name,
+              labelContent: places.name,
+              labelClass: "labels"
+            });
+
+          } else {
+            alert("No results found");
+          }
+        } else {
+          alert("Geocode was not successful for the following reason: " + status);
+        }
+      });
     });
   }
 }
